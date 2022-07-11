4480abd668355f1fea7fbb014aabadf0d118864c
Callum-OP <callumoprey@gmail.com>
2022-07-11 15:40:07 +0100

    Added controller allowing user to register and login

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,3 +18,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register',[AuthController::class,'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
